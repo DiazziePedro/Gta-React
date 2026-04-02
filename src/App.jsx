313b774d9393ac5ee6c113a54698fa6e3a414048
Luccas Diazzi <diazziti@gmail.com>
2026-04-02 11:35:51 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import logoGta from './assets/logo-gta.svg'
+import { Joystick, Gamepad2, GamepadDirectional } from 'lucide-react';
 
 export default function App() {
 
@@ -14,9 +15,9 @@ export default function App() {
         <footer>
           <small>@2026 ag2cti.com</small>
           <div id="social">
-            <div className="rs"></div>
-            <div className="rs"></div>
-            <div className="rs"></div>
+            <div className="rs"><Joystick className='icone'/></div>
+            <div className="rs"><Gamepad2 className='icone'/></div>
+            <div className="rs"><GamepadDirectional className='icone' /></div>
           </div>
         </footer>
       </main>
